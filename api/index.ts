5422fd5352524db3e814be417861c94b4699e025
alialridaalnajjar <alialridaalnajjar@gmail.com>
2025-12-27 14:40:59 +0200

    Add initial API setup with Express and Vercel configuration

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import registerRoute from "./routes/Register.route";
-import dataRoute from "./routes/Data.route";
-import activityRoute from "./routes/Activity.route";
-import videoRoute from "./routes/Video.route";
-import docsRoute from "./routes/Docs.route";
-import isNewRoute from "./routes/IsNew.route";
+import registerRoute from "../routes/Register.route";
+import dataRoute from "../routes/Data.route";
+import activityRoute from "../routes/Activity.route";
+import videoRoute from "../routes/Video.route";
+import docsRoute from "../routes/Docs.route";
+import isNewRoute from "../routes/IsNew.route";
 const corsOptions = {
   origin: "http://localhost:5173",
 };
@@ -23,6 +23,5 @@ app.use("/api/activity", activityRoute);
 app.use("/api/video", videoRoute);
 app.use("/api/docs", docsRoute);
 app.use("/api/isNew", isNewRoute);
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+export default app;
